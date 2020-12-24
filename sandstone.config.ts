5ac63dcbd6cd17d272329f3c5451b7707ef587fa
Mulverine Gaming <mulverin3@gmail.com>
2020-12-24 14:10:26 -0700

    Initial Commit

--- sandstone.config.ts
+++
@@ -1,9 +0,0 @@
-import { SandstoneConfig } from 'sandstone/core'
-
-export default {
-  name: 'smc-markdown',
-  description: [ 'A', { text: 'Sandstone', color: 'gold' }, ' data pack.' ],
-  formatVersion: 6,
-  namespace: 'markdown',
-  saveOptions: { path: './.pack' }
-} as SandstoneConfig
